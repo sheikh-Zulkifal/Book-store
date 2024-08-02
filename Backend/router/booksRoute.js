d89d863beec0248bd75d857410b49aee2d1763b2
sheikh-Zulkifal <sheikhzulkifal1@gmail.com>
2024-08-02 15:40:19 +0500

    fjbfv

--- Backend/router/booksRoute.js
+++ Backend/router/booksRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Book } from "../models/bookmodel.js";
+import { Book } from "../models/bookModel.js";
 
 const router = express.Router();
 router.post("/", async (req, res) => {
